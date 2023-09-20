9e2e06377d10b3f9538b6a550b0ac8e22d43846c
sharmila675 <sharmilajirel8@gmail.com>
2023-09-20 09:33:38 +0545

    part7-exercise

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,29 +2,30 @@ import Header from "./Header"
 import Content from "./Content"
 import Total from "./Total"
 
-
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name:'Fundamentals of React',
+  const course = {
+    name:'Half Stack application development',
+    parts : [
+  {
+  name:'Fundamentals of React',
   exercises :10
-  }
-
-  const part2 = {
-    name:'Using props to pass data',
+  },
+ {
+  name:'Using props to pass data',
   exercises : 7
-  }
-  const part3 = {
-    name:'State of a component',
+  },
+  {
+  name:'State of a component',
   exercises : 14
   }
+]
+}
 
   return (
     <div>
       <Header course={course} />
-      <Content ourcontent={part1} secondcontent={exercises1} thirdContent = {part2} fourthContent ={exercises2} fifthContent ={part3} lastContent ={exercises3} />
-      
-      <Total first={exercises1} second={exercises2} third={exercises3} /> 
+      <Content parts = {course} /> 
+      <Total parts = {parts} />
     </div>
   )
 }
---
+++ part1/unicafe/src/StatisticLine.js
@@ -0,0 +1,11 @@
+const Statistics = (props) => {
+    /// ...
+    return(
+      <div>
+        <StatisticLine text="good" value ={...} />
+        <StatisticLine text="neutral" value ={...} />
+        <StatisticLine text="bad" value ={...} />
+        // ...
+      </div>
+    )
+  }
